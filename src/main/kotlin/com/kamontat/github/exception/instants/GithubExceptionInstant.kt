15cd2112fb235b3eff537af2312b427734619ddc
kamontat <kamontat_c@hotmail.com>
2017-07-10 16:49:02 +0700

    add exception builder for builder exception 5555

--- src/main/kotlin/com/kamontat/github/exception/instants/GithubExceptionInstant.kt
+++ src/main/kotlin/com/kamontat/github/exception/instants/GithubExceptionInstant.kt
@@ -1,6 +1,7 @@
 package com.kamontat.github.exception.instants
 
 import com.beust.klaxon.JsonObject
+import com.kamontat.github.exception.BuilderObjectException
 import com.kamontat.github.exception.InvalidOrderException
 import com.kamontat.github.exception.MessageException
 import com.kamontat.github.exception.NetworkException
@@ -38,5 +39,15 @@ class GithubExceptionInstant {
         fun get(json: JsonObject, code: Int, throws: Throwable? = null): MessageException = MessageException(json, HttpCode.getByCode(code)[0], throws)
 
         fun get(json: JsonObject, code: HttpCode, throws: Throwable? = null): MessageException = MessageException(json, code, throws)
+    }
+
+    object BuilderError {
+        fun get(code: ErrorCode): BuilderObjectException = BuilderObjectException(code.toString())
+
+        fun get(code: ErrorCode, throws: Throwable): BuilderObjectException = BuilderObjectException(code.toString(), throws)
+
+        fun get(message: String): BuilderObjectException = BuilderObjectException(message)
+
+        fun get(message: String, throws: Throwable): BuilderObjectException = BuilderObjectException(message, throws)
     }
 }
