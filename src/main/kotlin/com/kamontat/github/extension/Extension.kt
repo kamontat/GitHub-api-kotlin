4bd1a911ec4dcec232d856bba49653e26737d21f
kamontat <kamontat_c@hotmail.com>
2017-07-10 16:49:48 +0700

    add more date parser

--- src/main/kotlin/com/kamontat/github/extension/Extension.kt
+++ src/main/kotlin/com/kamontat/github/extension/Extension.kt
@@ -1,13 +1,25 @@
 package com.kamontat.github.extension
 
 import com.beust.klaxon.JSON
-import com.beust.klaxon.JsonObject
+import com.beust.klaxon.JsonBase
 import com.beust.klaxon.Parser
+import java.text.SimpleDateFormat
+import java.util.*
 
-fun JSON.fromString(string: String): JsonObject {
-    return Parser().parse(StringBuilder(string)) as JsonObject
+
+fun JSON.fromString(string: String): JsonBase {
+    return fromStringBuilder(StringBuilder(string))
 }
 
-fun JSON.fromStringBuilder(stringBuilder: StringBuilder): JsonObject {
-    return Parser().parse(stringBuilder) as JsonObject
+fun JSON.fromStringBuilder(stringBuilder: StringBuilder): JsonBase {
+    return Parser().parse(stringBuilder) as JsonBase
+}
+
+fun Date.githubParser(timeStamp: String): Date? {
+    val format = arrayOf("yyyy/MM/dd HH:mm:ss ZZZZ", "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    format.forEach {
+        val dateFormat = SimpleDateFormat(it)
+        return dateFormat.parse(timeStamp)
+    }
+    return null
 }
