60666d879d93f2e24c85dc1b17f73b6252e833f1
kamontat <kamontat_c@hotmail.com>
2017-07-10 16:50:14 +0700

    Add log and Fixed some bug!

--- src/main/kotlin/com/kamontat/github/model/https/CURL.kt
+++ src/main/kotlin/com/kamontat/github/model/https/CURL.kt
@@ -1,5 +1,6 @@
 package com.kamontat.github.model.https
 
+import com.beust.klaxon.JsonBase
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import com.kamontat.github.exception.GithubException
@@ -7,27 +8,34 @@ import com.kamontat.github.exception.constants.ErrorCode
 import com.kamontat.github.exception.instants.GithubExceptionInstant
 import com.kamontat.github.extension.fromString
 import com.kamontat.github.model.link.GithubLink
+import com.kamontat.github.util.Log
 import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import java.net.URL
 import java.util.*
+import java.util.logging.Level
 
 /**
  * @author kamontat
  * @version 1.0
  * @since Tue 04/Jul/2017 - 3:34 PM
  */
-class CURL(private val method: RequestMethod, private val rawLink: GithubLink) {
-    private val link: URL
-        get() = URL(rawLink.get())
+class CURL(private val method: RequestMethod, private val rawLink: GithubLink, private val debug: Boolean = false) {
+    private val logger = Log(debug)
+
+    private var link: URL = URL(rawLink.get())
+
+    init {
+        logger.get()?.log(Level.INFO, "link: $link")
+    }
 
 
     private val client = OkHttpClient()
     private var request: Request? = null
 
-    private val requestBuilder: Request.Builder = Request.Builder().url(link).method(method.name, null)
+    private val requestBuilder: Request.Builder = Request.Builder().method(method.name, null)
 
     companion object {
         val JSON: MediaType? = MediaType.parse("application/json; charset=utf-8")
@@ -35,12 +43,16 @@ class CURL(private val method: RequestMethod, private val rawLink: GithubLink) {
 
     @Throws(GithubException::class)
     fun fetch(): String {
-        request = requestBuilder.build()
-        if (!isBuild()) throw GithubExceptionInstant.Invalid.get(ErrorCode.NEVER_BUILD_LINK)
+        logger.get()?.log(Level.INFO, "start fetch ($link)")
+        request = requestBuilder.url(link).build()
+
+        if (!isBuild())
+            throw GithubExceptionInstant.Invalid.get(ErrorCode.NEVER_BUILD_LINK)
         if (Objects.isNull(link) || Objects.isNull(request))
             throw GithubExceptionInstant.Network.get(ErrorCode.LINK_NOT_FOUND)
 
         val response = client.newCall(request).execute()
+
         if (Objects.isNull(response) && Objects.isNull(response.body()))
             throw GithubExceptionInstant.Network.get(ErrorCode.RESPONSE_NOT_FOUND)
         if (!response.isSuccessful)
@@ -50,7 +62,7 @@ class CURL(private val method: RequestMethod, private val rawLink: GithubLink) {
     }
 
     @Throws(GithubException::class)
-    fun fetchAsJSON(): JsonObject {
+    fun fetchAsJSON(): JsonBase {
         return com.beust.klaxon.JSON().fromString(fetch())
     }
 
